c7b4c761323536dafd9becd4f21fbdc03c940791
Adam Biser <adambiser@gmail.com>
2019-02-24 22:23:41 -0500

    Updated postbuild to check file exists

--- contrib/vs2015/adplug/postbuild.bat
+++ contrib/vs2015/adplug/postbuild.bat
@@ -3,9 +3,14 @@
 ECHO Copying header files to AdlibPlugin folder
 Copy "..\..\..\src\*.h" "..\..\..\..\agk-adlib-plugin\AdlibPlugin\AdPlug\src\*.h" > nul
 
-ECHO Copying static library to AdlibPlugin folder
-Copy "..\v140\Win32\Release\adplug.lib" "..\..\..\..\agk-adlib-plugin\AdlibPlugin\AdPlug\Win32\adplug.lib" > nul
+IF EXIST "..\v140\Win32\Release\adplug.lib" (
+	ECHO Copying RELEASE static library to AdlibPlugin folder
+	Copy "..\v140\Win32\Release\adplug.lib" "..\..\..\..\agk-adlib-plugin\AdlibPlugin\AdPlug\Win32\adplug.lib" > nul
+)
 
-ECHO Copying static library to AdlibPlugin folder
-Copy "..\v140\Win32\Debug\adplug.lib" "..\..\..\..\agk-adlib-plugin\AdlibPlugin\AdPlug\Win32\adplug-debug.lib" > nul
-Copy "..\v140\Win32\Debug\adplug.pdb" "..\..\..\..\agk-adlib-plugin\AdlibPlugin\AdPlug\Win32\adplug-debug.pdb" > nul
+IF EXIST "..\v140\Win32\Debug\adplug.lib" (
+	ECHO Copying DEBUG static library to AdlibPlugin folder
+	Copy "..\v140\Win32\Debug\adplug.lib" "..\..\..\..\agk-adlib-plugin\AdlibPlugin\AdPlug\Win32\adplug-debug.lib" > nul
+	Copy "..\v140\Win32\Debug\adplug.pdb" "..\..\..\..\agk-adlib-plugin\AdlibPlugin\AdPlug\Win32\adplug-debug.pdb" > nul
+)
+
